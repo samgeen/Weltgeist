660df0e0ef516703d77db548aa179f18ccdd2df4
Sam Geen <samgeen@gmail.com>
2018-01-02 17:29:58 +0000

    Allowed plotting of r vs t for Sedov soln

--- src/test.py
+++ src/test.py
@@ -23,8 +23,8 @@ class Integrator(object):
         self._my = 0.5
         self._step = True
         self._itick = 0
-        self._savex = [1e-7,1e-7]
-        self._savey = [1e-7,1e-7]
+        self._savex = []
+        self._savey = []
         
     def Setup(self):
 
@@ -43,39 +43,50 @@ class Integrator(object):
             
             glColor4f(1.0,1.0,1.0,0.5)
             #nx = hydro.ncells
-            nx = len(self._savex)
-            pts = np.zeros((nx,2),dtype="float64")
-            #pts[:,1] = hydro.vel[0:nx]*1e6
-            #pts[:,0] = hydro.x
-            #pts[:,1] = np.log10(hydro.T)
-            #print len(self._savex)
-            pts[:,0] = np.log10(np.array(self._savex))
-            pts[:,1] = np.log10(np.array(self._savey))
-            print pts[:,0].max(), pts[:,1].max()
+            nx = 100000 # len(self._savex)
+            sx = np.array(self._savex)
+            sy = np.array(self._savey)
+            try:
+                check = sx.min() != sx.max()
+            except:
+                check = False
+            if check:
+                pts = np.zeros((nx,2),dtype="float64")
+                #pts[:,1] = hydro.vel[0:nx]*1e6
+                #pts[:,0] = hydro.x
+                #pts[:,1] = np.log10(hydro.rho)
+                #print len(self._savex)
+                lx = len(sx)
+                pts[0:lx,0] = sx
+                pts[0:lx,1] = sy
+                pts[lx:,0] = pts[lx-1,0]
+                pts[lx:,1] = pts[lx-1,1]
+                #print pts[:,0].max(), pts[:,1].max()
+                #print pts[:,0]
 
-            glMatrixMode(GL_PROJECTION)
-            glLoadIdentity()
-            #float(vhone.data.xmin),float(vhone.data.xmax
-            # No idea what's wrong here if I don't do this... gah
-            xmin = pts[:,0].min()-1e-7
-            xmax = pts[:,0].max()+1e-7
-            ymin = pts[:,1].min()-0.5
-            ymax = pts[:,1].max()+0.5
-            glOrtho(xmin, xmax,
-                    ymin, ymax,
-                    -2, 2)
-            glMatrixMode(GL_MODELVIEW)
+                glMatrixMode(GL_PROJECTION)
+                glLoadIdentity()
+                #float(vhone.data.xmin),float(vhone.data.xmax
+                # No idea what's wrong here if I don't do this... gah
+                xmin = pts[:,0].min()-1e-7
+                xmax = pts[:,0].max()+1e-7
+                ymin = pts[:,1].min()-1e-7
+                ymax = pts[:,1].max()+1e-7
+                glOrtho(xmin, xmax,
+                        ymin, ymax,
+                        -2, 2)
+                glMatrixMode(GL_MODELVIEW)
 
-            #print "rho MAX/MIN:", vhone.data.zro.min(),vhone.data.zro.max()
-            #print "P MAX/MIN:", vhone.data.zpr.min(),vhone.data.zpr.max()
-            #print vhone.data.dt
-            #pts = rampy.data.xp
-            pts = pts.flatten()
-            self._glPoints = (GLdouble * len(pts))(*pts)
-            
-            glEnableClientState(GL_VERTEX_ARRAY)
-            glVertexPointer(2, GL_DOUBLE, 0, self._glPoints)
-            glDrawArrays(GL_LINE_STRIP,0,len(pts)//2)
+                #print "rho MAX/MIN:", vhone.data.zro.min(),vhone.data.zro.max()
+                #print "P MAX/MIN:", vhone.data.zpr.min(),vhone.data.zpr.max()
+                #print vhone.data.dt
+                #pts = rampy.data.xp
+                pts = pts.flatten()
+                self._glPoints = (GLdouble * len(pts))(*pts)
+                
+                glEnableClientState(GL_VERTEX_ARRAY)
+                glVertexPointer(2, GL_DOUBLE, 0, self._glPoints)
+                glDrawArrays(GL_LINE_STRIP,0,len(pts)//2)
             
         @self._window.event
         def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
@@ -102,6 +113,7 @@ class Integrator(object):
             
         # Initialise sim data
         init.init()
+        hydro.P[0] *= 1e8
         # Set up rendering
         s = 0.02
         #glOrtho(-s,+s,-s,+s,-100*s,+100*s)
@@ -126,14 +138,17 @@ class Integrator(object):
             #    vhone.data.zux[0,0,0] += 1e6*vhone.data.dt
             #    vhone.data.zro[0,0,0] += 1e2*vhone.data.dt
             hydro.Step()
-            self._savex.append(hydro.time)
             rho = hydro.rho[0:nx]
-            self._savey.append(hydro.x[np.where(rho == rho.max())][0])
-            if not sn and hydro.time > 0.00:
-                print "BANG"
-                hydro.P[0] *= 1e8
-                sn = True
-                print hydro.time, hydro.dt
+            rs = hydro.x[np.where(rho == rho.max())]
+            if (rs > 0):
+                self._savex.append(hydro.time+0.0)
+                self._savey.append(rs[0])
+            #print hydro.time, rs[0]
+            #if not sn and hydro.time > 0.00:
+            #    print "BANG"
+            #    hydro.P[0] *= 1e8
+            #    sn = True
+            #    print hydro.time, hydro.dt
         if self._itick > 10:
             self._itick = 0
         
