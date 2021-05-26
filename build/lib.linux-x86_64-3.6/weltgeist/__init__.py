c7d52e879bb3473cecbb06651b5e76dac3020da6
Sam Geen <samgeen@gmail.com>
2021-05-26 17:00:05 +0200

    Removed build and dist from repo

--- build/lib.linux-x86_64-3.6/weltgeist/__init__.py
+++
@@ -1,3 +0,0 @@
-# Decides what to import from this folder
-# NOTE: I removed "graphics" from this list because pyglet seems to segfault pdb on my machine
-from . import analyticsolutions, cooling, gravity, integrator, radiation, sources, units
