c12efb21e71af6a53d30e1592533d9ff1d8c1fd1
Sam Geen <samgeen@king.localdomain>
2022-09-28 10:12:03 +0200

    Fixed setup.py products to actually import

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='A 1D hydrodynamic code for spherical astrophysics',
     license="MIT",
     packages=['weltgeist'],
-    cmake_args=['-DSKBUILD=ON']
+    cmake_args=['-DSKBUILD=ON -fPIC']
 )
