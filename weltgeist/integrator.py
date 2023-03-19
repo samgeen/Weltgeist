000c69957625e0ddd6653a6741ce86e6833c209a
Sam Geen <samgeen@king.localdomain>
2023-03-19 21:20:12 +0100

    Fixed small bug in atTargetTime

--- weltgeist/integrator.py
+++ weltgeist/integrator.py
@@ -93,13 +93,13 @@ class Saver(object):
             return
         # Find the first time to save (if more than one method of saving is implemented)
         timeToSave = min(timesToSave)
-        atTimeTarget = False
+        atTargetTime = False
         if self._forceExactTimes:
             atTargetTime = integrator.ForceTimeTarget(timeToSave)
         # Add a paranoid check where either:
         # 1. the integrator reports hitting its target time, or
         # 2. the integrator's current time is past the time to save
-        if atTargetTime or Integrator().time >= timeToSave:
+        if atTargetTime or integrator.time >= timeToSave:
             self.Save()
 
 class _Integrator(object):
